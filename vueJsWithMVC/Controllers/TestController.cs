ba363f71cf67142ee6e10f07cb13ff8dfb098eb2
Eliseo Calderon <ecalderon@Nerium.com>
2020-01-08 22:47:02 -0600

    Add project files.

---
+++ vueJsWithMVC/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Http;
+
+namespace vueJsWithMVC.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Ok($"This is a new test message at {DateTime.Now}!");
+        }
+    }
+}
---
+++ vueJsWithMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="vueJsWithMVC.MvcApplication" Language="C#" %>
